08d81a786566e35887e54a6589f260df3ace9203
root <1530326@upv.edu.mx>
2018-02-27 23:43:10 +0000

    Proyecto Joomla

---
+++ administrator/logs/error.php
@@ -0,0 +1,12 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2018-02-11 06:43:05 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2018-02-11T06:43:05+00:00	INFO 187.198.158.184	joomlafailure	Username and password do not match or you do not have an account yet.
+2018-02-11T07:07:18+00:00	INFO 187.198.158.184	joomlafailure	El usuario y contraseña no coinciden o usted aún no tiene una cuenta.
+2018-02-11T07:07:27+00:00	INFO 187.198.158.184	joomlafailure	El usuario y contraseña no coinciden o usted aún no tiene una cuenta.
+2018-02-14T15:55:07+00:00	INFO 148.245.119.253	joomlafailure	El usuario y contraseña no coinciden o usted aún no tiene una cuenta.
+2018-02-15T05:08:23+00:00	INFO 189.218.189.25	joomlafailure	El usuario y contraseña no coinciden o usted aún no tiene una cuenta.
+2018-02-16T03:02:23+00:00	INFO 189.218.189.25	joomlafailure	El usuario y contraseña no coinciden o usted aún no tiene una cuenta.
---
+++ cache/com_virtuemart_cats/ffce9c9308b3cc1756ea8b6aeae8877b-cache-com_virtuemart_cats-76115b08ed40cabaf3e877e5015bf1ce.php
@@ -0,0 +1,1 @@
+<?php die("Access Denied"); ?>#x#a:2:{s:6:"output";s:0:"";s:6:"result";a:1:{i:0;O:8:"stdClass":8:{s:22:"virtuemart_category_id";s:2:"72";s:13:"category_name";s:6:"Musica";s:20:"category_description";s:12:"<p>padre</p>";s:8:"metadesc";s:0:"";s:7:"metakey";s:0:"";s:11:"customtitle";s:0:"";s:4:"slug";s:6:"musica";s:19:"virtuemart_media_id";b:0;}}}
